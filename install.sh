2ac7ec1e0ed7141f3d259015310b4e122499f045
Felipe Salvatore <felipessalvador@googlemail.com>
2016-06-22 18:16:26 -0300

    Add installation script for python packages

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt-get install git
+sudo apt install python-pip
+sudo apt install ipython
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt-get install build-essential python3-dev
+sudo apt-get install Kate
+sudo pip3 install sniffer
+sudo pip3 install pyinotify
