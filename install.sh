f4ad510b60f4e8de1519a4caad7502b1cdc9e175
Felsal <felipessalvador@googlemail.com>
2016-10-23 22:46:22 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,13 @@ sudo apt install python-pip
 sudo apt install ipython
 sudo apt install python3
 sudo apt install python3-pip
+pip3 install --upgrade pip3
 sudo apt-get install build-essential python3-dev
 sudo apt-get install Kate
 sudo pip3 install sniffer
 sudo pip3 install pyinotify
+sudo pip3 install jupyter
+sudo pip3 install nltk
+sudo pip3 install numpy
+sudo pip3 install scipy
+sudo pip3 install matplotlib
