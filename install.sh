9919fd428e4811261decb58829d4d1c04565e038
felipessalvatore <felipessalvatore@github.com>
2018-02-19 15:26:21 -0300

    add sublime settings

--- install.sh
+++ install.sh
@@ -29,3 +29,19 @@ sudo pip3 install gym
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
 sudo dpkg -i skypeforlinux-64.deb
 sudo apt install -f
+# python3 build for sublime
+sudo dd of=~/.config/sublime-text-3/Packages/User/python3.sublime-build << EOF
+{
+    "shell_cmd": "/usr/bin/env python3 ${file}",
+    "selector": "source.python",
+    "file_regex": "^(...*?):([0-9]*):?([0-9]*)",
+    "working_dir": "${file_path}",
+}
+EOF
+# sublime preferences
+sudo dd of=~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings << EOF
+{
+    "font_size": 13,
+    "translate_tabs_to_spaces": true
+}
+EOF
