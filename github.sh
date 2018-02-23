5a7d59fe709a72d388e633c4883f379a55c9a96f
felipessalvatore <felipessalvatore@github.com>
2018-02-23 17:29:25 -0300

    add script for setting github ssh key

---
+++ github.sh
@@ -0,0 +1,4 @@
+git config --global user.email "felipessalvatore@github.com"
+git config --global user.name "felipessalvatore"
+ssh-keygen -t rsa -b 4096 -C "felipessalvatore@github.com"
+subl ~/.ssh/id_rsa.pub
