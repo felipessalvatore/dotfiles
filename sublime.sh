9da99551c25eb6a2896b78dd41e308a65f4db83c
Felsal <felipessalvador@googlemail.com>
2019-05-11 19:15:37 -0300

    Update sublime.sh

--- sublime.sh
+++ sublime.sh
@@ -13,3 +13,13 @@ sudo dd of=~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings <
     "translate_tabs_to_spaces": true
 }
 EOF
+# autopep8 ublime preferences
+pip install autopep8
+sudo dd of=~/.config/sublime-text-3/Packages/User/autopep8.sublime-settings << EOF
+{
+    "shell_cmd": "autopep8 --aggressive --in-place ${file}",
+    "selector": "source.python",
+    "file_regex": "^(...*?):([0-9]*):?([0-9]*)",
+    "working_dir": "${file_path}",
+}
+EOF
